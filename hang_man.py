bcbceedf7d4d2b59f1330d460eec8e46bf37db57
CameronCode22 <131026245+CameronCode22@users.noreply.github.com>
2024-02-05 23:37:09 +0000

    Add files via upload

---
+++ hang_man.py
@@ -0,0 +1,17 @@
+#1. randomly selectinga word
+#2. the player has to guess a word
+# -loop through each letter of the guessed word
+# -compare the letter in the guessed word with the actal word
+
+#1. the letter guessed incorrect
+#2. the letter guessed is wrong
+#3. the letter guessed is correct but at wrong positio
+
+def wrong_letters():
+    pass
+def wrongPosition():
+    pass
+def play():
+
+print("hello")
+
